1620dd6eae474ace42cf483baaba4f89db23ce9f
Paschalis Skouzos <paschalis.skouzos@gmail.com>
2018-10-11 20:41:48 +0100

    rerun local

--- main.py
+++ main.py
@@ -3,11 +3,15 @@
 import sys
 import os
 
+COMBINER = True
 task_folder = os.path.join('task2')
 sys.path.append(task_folder)
 if not os.path.exists(os.path.join(task_folder, 'outputs')):
     os.makedirs(os.path.join(task_folder, 'outputs'))
-output_file = os.path.join(task_folder, 'outputs/local_comb.out')
+if COMBINER:
+    output_file = os.path.join(task_folder, 'outputs/local_comb.out')
+else:
+    output_file = os.path.join(task_folder, 'outputs/local.out')
 
 data_folder = os.path.join('exc_data', 'small', 'gutenberg')
 input_files = [os.path.join(data_folder, '8830902613096153013.txt'),
@@ -31,14 +35,15 @@ with open('pipe.txt', 'r') as sys.stdin, open('sorted_pipe.txt', 'w') as sys.std
         sys.stdout.write(entry)
     sys.stdout.flush()
 
-with open('sorted_pipe.txt', 'r') as sys.stdin, open('comb_pipe.txt', 'w') as sys.stdout:
-    import combiner
+if COMBINER:
+    with open('sorted_pipe.txt', 'r') as sys.stdin, open('comb_pipe.txt', 'w') as sys.stdout:
+        import combiner
 
-with open('comb_pipe.txt') as sys.stdin, open(output_file, 'w') as sys.stdout:
-    import reducer
-
-# with open('sorted_pipe.txt') as sys.stdin, open(output_file, 'w') as sys.stdout:
-#     import reducer
+    with open('comb_pipe.txt') as sys.stdin, open(output_file, 'w') as sys.stdout:
+        import reducer
+else:
+    with open('sorted_pipe.txt') as sys.stdin, open(output_file, 'w') as sys.stdout:
+        import reducer
 
 sys.stdin = sys.__stdin__
 sys.stdout = sys.__stdout__
