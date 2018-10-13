eec12cd3dd966ff445b0c57f3b4ef3c4944e1222
Paschalis Skouzos <paschalis.skouzos@gmail.com>
2018-10-13 07:08:17 +0100

    fixed????

--- task8/reducer.py
+++ task8/reducer.py
@@ -10,20 +10,25 @@ for line in sys.stdin:  # For every line in the input from stdin
     line = line.strip()  # Remove trailing characters
     title_id, value = line.split('\t', 1)
 
-    # We are reading lines ordered by 1) title_id and 2) year since 'y' > [0-9]
-    # the line with the year will be sorted before the one with the rating
+    # We are reading lines ordered by title_id
     if prev_title_id == title_id:
         # If we found two equal titles then we have both the year and the
-        # rating of the title so we are printing. We have the year from the
-        # previous line and we are now getting the rating
-        rating = value.split('\t')[1]
-        print(year + '\t' + rating)
-    else:
-        # If it's the first line of this title and it's a rating line it means
-        # that we don't have the year of this title so we just move on.
-        # Note that a rating's line has 'y' in the year column
+        # rating of the title so we are printing. If we read the the rating
+        # in the previous line we are now reading the year and vice versa.
         if value.split('\t')[0] != 'y':
             year = value.split('\t')[0]
+        else:
+            rating = value.split('\t')[1]
+        print(year + '\t' + rating)
+    else:
+        # If the first line of the new title_id is the year we are saving the
+        # year+resetting rating to None and vice versa
+        if value.split('\t')[0] != 'y':
+            year = value.split('\t')[0]
+            rating = None
+        else:
+            rating = value.split('\t')[1]
+            year = None
 
         prev_title_id = title_id
 
