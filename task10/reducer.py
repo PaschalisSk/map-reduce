20048bab939bc19ce21dd480f68839fa610d7fda
Paschalis Skouzos <paschalis.skouzos@gmail.com>
2018-10-15 01:01:05 +0100

    Finished task10

--- task10/reducer.py
+++ task10/reducer.py
@@ -3,32 +3,27 @@
 import sys
 
 prev_title_id = None
+# The lines with titles within our year range (2010-2018)
+# are sorted before the ones with crew id because they have 'c' in the second
+# column. We can therefore check the year column for a new title_id and if
+# the column has value='c' then this movie was released 2010-2018.
+valid_year = False
 
 for line in sys.stdin:  # For every line in the input from stdin
-    #title_id, value = line.strip().split('\t', 1)
-    print(line.strip())
-    # # We are reading lines ordered by title_id
-    # if prev_title_id == title_id:
-    #     # If we found two equal titles then we have both the year and the
-    #     # rating of the title so we are printing. If we read the the rating
-    #     # in the previous line we are now reading the year and vice versa.
-    #     if value.split('\t')[0] != 'y':
-    #         year = value.split('\t')[0]
-    #     else:
-    #         rating = value.split('\t')[1]
-    #     print(year + '\t' + rating)
-    # else:
-    #     # If the first line of the new title_id is the year we are saving the
-    #     # year and resetting rating to None and vice versa
-    #     if value.split('\t')[0] != 'y':
-    #         year = value.split('\t')[0]
-    #         rating = None
-    #     else:
-    #         rating = value.split('\t')[1]
-    #         year = None
-    #
-    #     prev_title_id = title_id
+    title_id, crew_id = line.strip().split('\t', 1)
 
-# We can't miss any pairs so there is nothing here.
-# If a pair is to be printed, it will be printed inside the
-# if statement which checks "prev_title_id == title_id"
+    # We are reading lines ordered by 1)title_id and 2)crew_id
+    if prev_title_id == title_id:
+        if valid_year:
+            print(crew_id + '\t')
+    else:
+        if crew_id == 'c':
+            valid_year = True
+        else:
+            valid_year = False
+
+        prev_title_id = title_id
+
+# We can't miss any crew_id so there is nothing here.
+# If a crew_id is to be printed, it will be printed inside the
+# if statement which checks for valid_year
