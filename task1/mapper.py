2798ece5869b0380f4ec85890678c77294d1e367
Paschalis Skouzos <paschalis.skouzos@gmail.com>
2018-10-27 15:29:35 +0100

    Added comment to mapper1

--- task1/mapper.py
+++ task1/mapper.py
@@ -16,3 +16,12 @@ print("words" + "\t" + str(words))
 
 # Both in this and some of the next tasks we implement a combiner
 # only to combine results from multiple mappers in one machine.
+
+# We could add a combiner in order to combine outputs from different
+# mappers which run in the same machine. However, the large Gutenberg dataset
+# has only 17 files which means the best increase we could get from a
+# reducer is if those 17 mappers(1 for each file) run on the same machine.
+# i.e. In the best case, a machine would shuffle & short 2 key-pair values
+# instead of 34(each mapper outputs 2 key-pair values). It is an infinitesimal
+# increase for this dataset so we didn't implement a combiner.
+
